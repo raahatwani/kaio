cc01c8929da28f3438d701bab4cdbb1a0d897e56
Raahat Wani <raahatwani@gmail.com>
2023-08-24 23:48:34 +0530

    modified:   lib/Culture/Gallery.dart 	new file:   lib/data.json 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/Culture/Gallery.dart
+++ lib/Culture/Gallery.dart
@@ -1,30 +1,48 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class GalleryPage extends StatelessWidget {
-  final List<String> imagePaths = [
-    'assets/gulmarg.jpg',
-    'assets/gulmarg.jpg',
-    'assets/gulmarg.jpg',
-    'assets/gulmarg.jpg',
-    'assets/gulmarg.jpg',
-    'assets/gulmarg.jpg',
-    'assets/gulmarg.jpg',
-    'assets/gulmarg.jpg',
-    'assets/gulmarg.jpg',
-    'assets/gulmarg.jpg',
-    'assets/gulmarg.jpg',
-    'assets/gulmarg.jpg',
-  ];
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class GalleryPage extends StatefulWidget {
+  @override
+  
+  State<GalleryPage> createState() => _GalleryPageState();
+}
+
+class _GalleryPageState extends State<GalleryPage> {
+
+late http.Response apiResp;
+var imgData;
+String url='https://api.unsplash.com/photos/?client_id=HDun6AM7gvQPnGI-B6ei8xIaD5F6Y02ZPUUwDRYDkxU';
+
+List data=[];
+List<String> imgUrl=[];
+getData() async {
+  apiResp = await http.get(Uri.parse(url));
+  data = jsonDecode(apiResp.body);
+  _assign();
+  
+
+}
+
+ _assign(){
+  for (var i = 0; i < data.length; i++) {
+    imgUrl.add(data.elementAt(i)["urls"]["regular"]);
+    
+  }
+  
+ }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: GridView.builder(
-        itemCount: imagePaths.length,
+        itemCount: data.length,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
+          crossAxisCount: 3,
           crossAxisSpacing: 10.0,
           mainAxisSpacing: 10.0,
         ),
@@ -34,13 +52,13 @@ class GalleryPage extends StatelessWidget {
             child: Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                  image: AssetImage(imagePaths[index]),
+                 image: DecorationImage(
+                  image: NetworkImage(imgUrl.elementAt(index)),
                   fit: BoxFit.cover,
-                ),
+               
               ),
             ),
-          );
+          ));
         },
       ),
     );
